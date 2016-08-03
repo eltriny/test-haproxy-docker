e1b77c4e43969fe7308d5e196c3afcd914a29f31
GooGyoJun <eltriny@etlrinys-MacBook.local>
2016-08-03 16:47:19 +0900

    mysql config

--- pushApp.sh
+++ pushApp.sh
@@ -1,2 +1,2 @@
 # stackato push --no-prompt --mem 512 --as testtomcat --docker-image eltriny/test_tomcat:v1
-stackato push --no-prompt --mem 512 --as testproxy --docker-image eltriny/test-haproxy-docker
+stackato push --no-prompt --mem 512 --as ngrinder --docker-image eltriny/test-haproxy-docker
