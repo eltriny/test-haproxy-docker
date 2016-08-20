22a94f356c2ea45dfe49fd32844399c7a2a00c73
GooGyoJun <eltriny@etlrinys-MacBook.local>
2016-08-20 19:39:28 +0900

    change pinpoint config

--- pushApp.sh
+++ pushApp.sh
@@ -1,2 +1,2 @@
 # stackato push --no-prompt --mem 512 --as testtomcat --docker-image eltriny/test_tomcat:v1
-stackato push --no-prompt --mem 512 --as tde --docker-image eltriny/test-haproxy-docker
+stackato push --no-prompt --mem 512 --as pinpoint --docker-image eltriny/test-haproxy-docker
