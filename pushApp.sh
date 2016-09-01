782b0b5cf385b6eb3d375211c88a12645192bf56
GooGyoJun <eltriny@eltrinys-MacBook.local>
2016-09-01 15:57:18 +0900

    cadvisor

--- pushApp.sh
+++ pushApp.sh
@@ -1,3 +1,4 @@
 # stackato push --no-prompt --mem 512 --as testtomcat --docker-image eltriny/test_tomcat:v1
-stackato push --no-prompt --mem 512 --as pinpoint --docker-image eltriny/test-haproxy-docker
-# stackato push --no-prompt --mem 512 --as stash-setup --docker-image eltriny/test-haproxy-docker
+# stackato push --no-prompt --mem 512 --as pinpoint --docker-image eltriny/test-haproxy-docker
+# stackato push --no-prompt --mem 512 --as pinpoint --docker-image eltriny/test-haproxy-docker
+stackato push --no-prompt --mem 512 --as tde --docker-image eltriny/test-haproxy-docker
