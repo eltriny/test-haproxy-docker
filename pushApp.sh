d961b887690ec843a39197992cc987a65f535a74
GooGyoJun <eltriny@eltrinys-MacBook.local>
2016-09-19 18:47:29 +0900

    test elastic search

--- pushApp.sh
+++ pushApp.sh
@@ -1,5 +1,5 @@
 # stackato push --no-prompt --mem 512 --as testtomcat --docker-image eltriny/test_tomcat:v1
 # stackato push --no-prompt --mem 512 --as pinpoint --docker-image eltriny/test-haproxy-docker
-# stackato push --no-prompt --mem 512 --as pinpoint --docker-image eltriny/test-haproxy-docker
-stackato push --no-prompt --mem 512 --as zabbix-old --docker-image eltriny/test-haproxy-docker
+stackato push --no-prompt --mem 512 --as tde --docker-image eltriny/test-haproxy-docker
+# stackato push --no-prompt --mem 512 --as zabbix-old --docker-image eltriny/test-haproxy-docker
 # stackato push --no-prompt --mem 512 --as cadvisor-40 --docker-image eltriny/test-haproxy-docker
