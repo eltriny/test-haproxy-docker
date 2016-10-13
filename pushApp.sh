66643044d03941a400d1dc843cfda0225ef0ccb8
GooGyoJun <eltriny@eltrinys-MacBook.local>
2016-10-13 16:29:56 +0900

    add tde sonarqube

--- pushApp.sh
+++ pushApp.sh
@@ -3,4 +3,5 @@
 # stackato push --no-prompt --mem 512 --as zabbix-old --docker-image eltriny/test-haproxy-docker
 # stackato push --no-prompt --mem 512 --as tde --docker-image eltriny/test-haproxy-docker
 # stackato push --no-prompt --mem 512 --as tde-elastic --docker-image eltriny/test-haproxy-docker
-stackato push --no-prompt --mem 512 --as tde-kibana --docker-image eltriny/test-haproxy-docker
+# stackato push --no-prompt --mem 512 --as tde-kibana --docker-image eltriny/test-haproxy-docker
+stackato push --no-prompt --mem 512 --as metatron-dev --docker-image eltriny/test-haproxy-docker
