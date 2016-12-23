70b839e14b693e9024f84c6f03be551d9642f34c
GooGyoJun <eltriny@eltrinys-MacBook.local>
2016-12-23 14:40:02 +0900

    pinpoint

--- pushApp.sh
+++ pushApp.sh
@@ -1,7 +1,8 @@
 # stackato push --no-prompt --mem 512 --as testtomcat --docker-image eltriny/test_tomcat:v1
 # stackato push --no-prompt --mem 512 --as pinpoint --docker-image eltriny/test-haproxy-docker
 # stackato push --no-prompt --mem 512 --as zabbix-old --docker-image eltriny/test-haproxy-docker
-stackato push --no-prompt --mem 512 --as tde --docker-image eltriny/test-haproxy-docker
+# stackato push --no-prompt --mem 512 --as tde --docker-image eltriny/test-haproxy-docker
 # stackato push --no-prompt --mem 512 --as tde-elastic --docker-image eltriny/test-haproxy-docker
 # stackato push --no-prompt --mem 512 --as tde-kibana --docker-image eltriny/test-haproxy-docker
 # stackato push --no-prompt --mem 512 --as metatron-dev --docker-image eltriny/test-haproxy-docker
+stackato push --no-prompt --mem 512 --as zabbix --docker-image eltriny/test-haproxy-docker
